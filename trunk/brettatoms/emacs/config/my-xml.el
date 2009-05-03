dfff6aebd02a33bf4de8455d2bda2802d444f479
brettatoms <brettatoms@9038a3df-323a-0410-badd-376414d00a92>
2009-05-03 20:55:00 +0000

    - update

--- trunk/brettatoms/emacs/config/my-xml.el
+++ trunk/brettatoms/emacs/config/my-xml.el
@@ -1,45 +1,55 @@
 ; -*- lisp -*-
 
-(add-to-list 'load-path (concat package-dir "nxml-mode"))
+;; ;****************
+;; ; XML and HTML Modes
+;; ;****************
 
-;****************
-; XML Mode
-;****************
+; load nxml
 (add-to-list 'load-path (concat package-dir "nxml-mode"))
 (load "rng-auto.el")
-(add-to-list 'auto-mode-alist '("\\.html$" . nxml-mode))
+
+(load (concat package-dir "nxhtml/autostart.el"))
+(add-hook 'nxml-mode-hook
+	  (lambda ()
+	    (flymake-mode 0)))
+
+(add-hook 'nxhtml-mode-hook
+	  (lambda ()
+	    (flymake-mode 0)))
+
+(add-hook 'xml-mode-hook
+	  (lambda ()
+	    (flymake-mode 0)))
+
+
+;; ;
+;; ; XSL mode (xslide)
+;; ;
+;; (add-to-list 'load-path (concat package-dir "xslide-0.2.2"))
+;; (autoload 'xsl-mode "xslide" "Major mode for XSL stylesheets." t)
+
+;; ;; Uncomment if you want to use `xsl-grep' outside of XSL files.
+;; ;(autoload 'xsl-grep "xslide" "Grep for PATTERN in files matching FILESPEC." t)
+
+;; ;; Uncomment if you want to use `xslide-process' in `xml-mode'.
+;; ;(autoload 'xsl-process "xslide-process" "Process an XSL stylesheet." t)
+;; ;(add-hook 'xml-mode-hook
+;; ;	  (lambda ()
+;; ;	    (define-key xml-mode-map [(control c) (meta control p)]
+;; ;	      'xsl-process)))
+
+;; ;; Turn on font lock when in XSL mode
+;; (add-hook 'xsl-mode-hook
+;; 	  'turn-on-font-lock)
+
+;; set modes by extension
 (setq auto-mode-alist
       (append
        (list
-	'("\\.html" . nxml-mode)
 	'("\\.xml" . nxml-mode)
-	'("\\.xhtml" . nxml-mode)
-	'("\\.rng" . nxml-mode))
-       auto-mode-alist))
-
-;
-; XSL mode (xslide)
-;
-(add-to-list 'load-path (concat package-dir "xslide-0.2.2"))
-(autoload 'xsl-mode "xslide" "Major mode for XSL stylesheets." t)
-
-;; Uncomment if you want to use `xsl-grep' outside of XSL files.
-;(autoload 'xsl-grep "xslide" "Grep for PATTERN in files matching FILESPEC." t)
-
-;; Uncomment if you want to use `xslide-process' in `xml-mode'.
-;(autoload 'xsl-process "xslide-process" "Process an XSL stylesheet." t)
-;(add-hook 'xml-mode-hook
-;	  (lambda ()
-;	    (define-key xml-mode-map [(control c) (meta control p)]
-;	      'xsl-process)))
-
-;; Turn on font lock when in XSL mode
-(add-hook 'xsl-mode-hook
-	  'turn-on-font-lock)
-
-(setq auto-mode-alist
-      (append
-       (list
+	'("\\.rng" . nxml-mode)
+	'("\\.xhtml" . nxhtml-mode)
+	'("\\.html" . nxhtml-mode)
 	'("\\.fo" . xsl-mode)
 	'("\\.xsl" . xsl-mode))
        auto-mode-alist))
